92d5b053bc702dfe08f6aaa814bb31acad94c735
Christian <kristian2015ok@gmail.com>
2026-03-08 16:07:35 +0700

    refactor(layout): remove ThemeProviderClient and simplify layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import ThemeProviderClient from "@/components/theme-provider";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -23,10 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${poppins.className} antialiased`}>
-        <ThemeProviderClient>
-          {children}
-          <Toaster />
-        </ThemeProviderClient>
+        {children}
+        <Toaster />
       </body>
     </html>
   );
